69abb96891980af44bcbff96ab81541144794fc2
ArshDauwa <Arshdauwa04@gmail.com>
2024-05-02 22:23:16 +0000

    Updated and fixed a few unit test

--- final-project/test/test_api.py
+++ final-project/test/test_api.py
@@ -27,24 +27,30 @@ def test_delete_data():
     # Load the data back for other tests
     requests.post(f'{base_url}/data')
 
-def test_get_gene_ids():
-    response = requests.get(f'{base_url}/genes')
+def test_get_exoplanets():
+    response = requests.get(f'{base_url}/exoplanets')
     assert response.status_code == 200
     data = response.json()
     assert isinstance(data, list)
 
-def test_get_gene_data():
-    response = requests.get(f'{base_url}/genes')
-    gene_ids = response.json()
-    if gene_ids:
-        gene_id = gene_ids[0]
-        response = requests.get(f'{base_url}/genes/{gene_id}')
+def test_get_exoplanet_data():
+    response = requests.get(f'{base_url}/exoplanets')
+    pl_names = response.json()
+    if pl_names:
+        pl_name = pl_names[0]
+        response = requests.get(f'{base_url}/exoplanets/{pl_name}')
         assert response.status_code == 200
         data = response.json()
         assert isinstance(data, dict)
-        assert 'hgnc_id' in data
+        assert 'pl_name' in data
     else:
         pytest.skip("No gene IDs available for testing")
+
+def get_exoplanets_by_radius():
+    response = requests.get(f'{base_url}/exoplanets')
+    
+
+
 
 def test_submit_job():
     job_data = {'start': 1, 'end': 10}
